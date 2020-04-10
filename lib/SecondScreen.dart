04fa65d9fe351a3cf85f2f5e1ffaab97f342a7ad
Uzair Leo <uzair.jan336@gmail.com>
2020-04-10 20:35:30 +0500

    add second screen

---
+++ lib/SecondScreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child:RaisedButton(
+          child: Text("Back to first Screen"),
+          onPressed: (){
+            Navigator.of(context).pop();
+          })
+      ),
+      
+    );
+  }
+}
